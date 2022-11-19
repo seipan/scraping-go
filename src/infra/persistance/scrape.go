511f439f5b528fc55c2056279f2ec67f006745d4
yamasaki <yamasakipann0218@gmail.com>
2022-11-19 23:12:59 +0900

    db connect

--- src/infra/persistance/scrape.go
+++ src/infra/persistance/scrape.go
@@ -1,1 +1,24 @@
 package persistance
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/seipan/scraping-go/utils"
+)
+
+func NewDriver() (*sql.DB, error) {
+	dbDSN, err := utils.DSN()
+	if err != nil {
+		return nil, err
+	}
+
+	db, err := sql.Open("mysql", dbDSN)
+	if err != nil {
+		log.Println("db connect failed")
+		panic(err)
+	}
+	log.Println("db connect success")
+
+	return db, nil
+}
---
+++ src/utils/config.go
@@ -0,0 +1,38 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+)
+
+func DSN() (string, error) {
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbDatabase := os.Getenv("DB_DATABASE")
+
+	if dbUser == "" || dbPassword == "" || dbHost == "" || dbPort == "" || dbDatabase == "" {
+		return "", fmt.Errorf("ERROR : required environment variable not found")
+	}
+	return fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?",
+		dbUser,
+		dbPassword,
+		dbHost,
+		dbPort,
+		dbDatabase,
+	) + "parseTime=true&collation=utf8mb4_bin", nil
+}
+
+func Port() string {
+	return GetEnvOrDefault("PORT", "8080")
+}
+
+func GetEnvOrDefault(envPath string, defaultEnv string) string {
+	env := os.Getenv(envPath)
+	if env == "" {
+		return defaultEnv
+	}
+	return env
+}
