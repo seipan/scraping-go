4d7ba32dc67d9ac039ab54a82f1476ae50cc5c2a
yamasaki <yamasakipann0218@gmail.com>
2022-11-22 02:00:53 +0900

    sql 変更

--- database/mysql/sql/1_create_article_table.sql
+++ database/mysql/sql/1_create_article_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `articles` (
-  `id`       int          COLLATE utf8mb4_bin NOT NULL AUTO_INCREMENT,
+  `id`       varchar(255) COLLATE utf8mb4_bin NOT NULL,
   `title`    varchar(255) COLLATE utf8mb4_bin NOT NULL,
-  `img_url`  varchar(255) COLLATE utf8mb4_bin NOT NULL,
-  `content`  varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `body`     varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `url`      varchar(255) COLLATE utf8mb4_bin NOT NULL,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
---
+++ database/mysql/sql/2_insert_article_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO articles (`id`, `title`, `body`,`url`) VALUES ("c1", "title1", "hello!","https://hoge.png");
+INSERT INTO articles (`id`, `title`, `body`,`url`) VALUES ("c2", "title2", "hello!","https://hoge1.png");
+INSERT INTO articles (`id`, `title`, `body`,`url`) VALUES ("c3", "title3", "hello!","https://hoge2.png");
