339cb3936beabb707ca0da104640d15ba27bda2c
yamasaki <yamasakipann0218@gmail.com>
2022-11-28 03:10:39 +0900

    scraoing

--- src/presentation/scrape.go
+++ src/presentation/scrape.go
@@ -1,6 +1,40 @@
 package presentation
 
-import "time"
+import (
+	"encoding/json"
+	"log"
+	"net/url"
+	"strconv"
+	"time"
+
+	"github.com/seipan/scraping-go/domain"
+	"github.com/seipan/scraping-go/scrape"
+)
+
+func GetQiitaArticle(page int, per_page int) *domain.Article {
+	endpoint := "http://qiita.com/api/v2/users/snaka/items"
+
+	values := url.Values{}
+
+	values.Set("page", strconv.Itoa(page))
+	values.Add("per_page", strconv.Itoa(per_page))
+
+	data, err := scrape.DoAPI("GET", endpoint, values, nil)
+	if err != nil {
+		log.Println("DoAPI Error() = %w", err)
+		return nil
+	}
+
+	Respon := &domain.Article{}
+
+	if err := json.Unmarshal(data, &Respon); err != nil {
+		log.Println("DoAPI Error() = %w", err)
+		return nil
+	}
+
+	return Respon
+
+}
 
 type Article struct {
 	Body       string    `json:"body"`
