c6ee5a1722ae71b8d6b034b3495cf8478319291a
yamasaki <yamasakipann0218@gmail.com>
2022-11-28 21:39:01 +0900

    main関数

--- src/main.go
+++ src/main.go
@@ -1,11 +1,36 @@
 package main
 
-import "log"
+import (
+	"log"
+	"sync"
 
-func GetParallelQiitaArticle(page int, per_page int) {
+	"github.com/seipan/scraping-go/domain"
+	"github.com/seipan/scraping-go/presentation"
+)
+
+func GetParallelQiitaArticle(page int, per_page int) *[]domain.Article {
 	log.Println("---------------------------")
+	page = 100
 
-	
+	articleChan := make(chan []presentation.Article, per_page*page)
+	var wg sync.WaitGroup
+
+	for i := 0; i < page; i++ {
+		wg.Add(1)
+		go presentation.GetParallelQiitaArticle(i, per_page, articleChan)
+		wg.Done()
+	}
+
+	wg.Wait()
+
+	var ResArticle []*domain.Article
+
+	for articles := range articleChan {
+		for _, article := range articles {
+			ResArticle = append(ResArticle, articles)
+
+		}
+	}
 }
 
 func main() {
