3b3195f0cbbc84fe04b5e86ef879d8b2a8e4b866
yamasaki <yamasakipann0218@gmail.com>
2022-11-28 22:58:45 +0900

    measure

--- src/utils/measure.go
+++ src/utils/measure.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 )
 
@@ -11,4 +12,14 @@ func measurer(fnc func()) time.Duration {
 	fnc()
 	end := time.Now()
 	return end.Sub(start)
+}
+
+func CalcAvgRuntime(target func(), N int) time.Duration {
+	var avgRuntime time.Duration
+	for i := 0; i < N; i++ {
+		fmt.Printf("\n%s time\n", strconv.Itoa(i+1))
+		avgRuntime += measurer(target)
+	}
+	avgRuntime = avgRuntime / time.Duration(int64(N))
+	return avgRuntime
 }
