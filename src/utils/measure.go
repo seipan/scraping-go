f60c8016f78820a0bcd9259545c3b0040f548e04
yamasaki <yamasakipann0218@gmail.com>
2022-11-29 20:41:46 +0900

    name change

--- src/utils/measure.go
+++ src/utils/measure.go
@@ -14,12 +14,13 @@ func measurer(fnc func()) time.Duration {
 	return end.Sub(start)
 }
 
-func CalcAvgRuntime(target func(), N int) time.Duration {
-	var avgRuntime time.Duration
+func CalcTime(kansu func(), N int) time.Duration {
+	var avgtime time.Duration
+
 	for i := 0; i < N; i++ {
 		fmt.Printf("\n%s time\n", strconv.Itoa(i+1))
-		avgRuntime += measurer(target)
+		avgtime += measurer(kansu)
 	}
-	avgRuntime = avgRuntime / time.Duration(int64(N))
-	return avgRuntime
+	avgtime = avgtime / time.Duration(int64(N))
+	return avgtime
 }
