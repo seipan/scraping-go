76c9a07f50a12944e899969824848a372dd2a05f
yamasaki <yamasakipann0218@gmail.com>
2022-11-29 20:44:06 +0900

    domain 消したC

--- src/main.go
+++ src/main.go
@@ -4,7 +4,6 @@ import (
 	"log"
 	"sync"
 
-	"github.com/seipan/scraping-go/domain"
 	"github.com/seipan/scraping-go/presentation"
 	"github.com/seipan/scraping-go/utils"
 )
@@ -25,13 +24,12 @@ func GetParallelQiitaArticle() {
 
 	wg.Wait()
 
-	var ResArticle []*domain.Article
+	var ResArticle []presentation.Article
 
 	for articles := range articleChan {
 		for _, article := range articles {
 			log.Println("GetArticle   " + article.Title)
-			domArticle := utils.JsonToDomain(article)
-			ResArticle = append(ResArticle, &domArticle)
+			ResArticle = append(ResArticle, article)
 		}
 	}
 
